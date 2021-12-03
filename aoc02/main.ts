c108e14371f8445a4ff5000244d1ad3a4944eac7
CJ <cjting1128@gmail.com>
2021-12-03 15:05:41 +0800

    Finish day 2

---
+++ aoc02/main.ts
@@ -0,0 +1,100 @@
+import { readAllSync } from "https://deno.land/std@0.116.0/streams/conversion.ts"
+import { assert } from "https://deno.land/std@0.116.0/testing/asserts.ts"
+
+interface Command {
+  type: "up" | "down" | "forward"
+  value: number
+}
+
+function isValidCommandType(type: string): type is Command["type"] {
+  return ["up", "down", "forward"].includes(type)
+}
+
+function parseLine(line: string): Command {
+  const [type, val] = line.split(" ")
+
+  if (isValidCommandType(type)) {
+    return {
+      type,
+      value: Number(val),
+    }
+  }
+
+  throw new Error(`invalid line: ${line}`)
+}
+
+// input is not so big, a few K lines, so a full read is not a big deal
+const input = new TextDecoder().decode(readAllSync(Deno.stdin))
+const cmds = input.trim().split("\n").map(parseLine)
+
+// depth: 786, horizontal position: 2199, result: 1728414
+// await part1(cmds)
+
+// aim: 786, depth: 802965, horizontal position: 2199, result: 1765720035
+await part2(cmds)
+
+async function part1(cmds: Command[]) {
+  let depth = 0
+  let pos = 0
+
+  for (const cmd of cmds) {
+    switch (cmd.type) {
+      case "up":
+        {
+          depth -= cmd.value
+        }
+        break
+
+      case "down":
+        {
+          depth += cmd.value
+        }
+        break
+
+      case "forward":
+        {
+          pos += cmd.value
+        }
+        break
+    }
+  }
+
+  console.log(
+    `depth: ${depth}, horizontal position: ${pos}, result: ${depth * pos}`
+  )
+}
+
+async function part2(cmds: Command[]) {
+  let pos = 0
+  let aim = 0
+  let depth = 0
+
+  for (const cmd of cmds) {
+    switch (cmd.type) {
+      case "up":
+        {
+          aim -= cmd.value
+        }
+        break
+
+      case "down":
+        {
+          aim += cmd.value
+        }
+        break
+
+      case "forward":
+        {
+          pos += cmd.value
+          depth += aim * cmd.value
+        }
+        break
+    }
+  }
+
+  console.log(
+    `aim: ${aim}, depth: ${depth}, horizontal position: ${pos}, result: ${
+      depth * pos
+    }`
+  )
+}
